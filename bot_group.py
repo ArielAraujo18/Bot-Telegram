50e24f4fac6a5a9c24a498e7e36f8f78a331e869
Ariel Araújo <142962703+ArielAraujo18@users.noreply.github.com>
2024-10-06 09:35:45 -0300

    Bot sinais
    Funcionando 100%, faltando apenas programar as funções

--- bot_group.py
+++ bot_group.py
@@ -6,17 +6,37 @@ token = '7576094527:AAHfjv4RLIZClltDwBgZhJPjJDI6OzruUhc'
 bot = telebot.TeleBot(token)
 
 #Id da conversa
-chat_id = '1884768237'
+chat_id = '-4504812852'
 
+
+sinais_teste = """🚥 SINALZINHO GRATUITO 🚥
+
+• EURGBP (OTC) - PUT 🟥  - 19:35
+• Expiração: 5 minutos [M5]
+• Se perder, fazer até 1 Gale."""
+#Exemplo de mensagens para serem programadas
+
+sinais_teste_outro = """🚥 SINALZINHO GRATUITO 🚥
+
+• EURUSD (OTC) - CALL 🟩  - 20:15
+• Expiração: 5 minutos [M5]
+• Se perder, fazer até 1 Gale.
+""" #Exemplo de mensagens para serem programadas
 
 # Função que envia a mensagem repetidamente
 def send_auto_message():
-    bot.send_message(chat_id,"Mensagem automática!")
+    bot.send_message(chat_id, sinais_teste_outro)
+
+def mennsagem():
+    bot.send_message(chat_id, sinais_teste)
+
 
 while True:
     send_auto_message()
     time.sleep(10)
+    mennsagem()
+    time.sleep(10)
 
-    
+
 # Inicia o envio
 bot.polling()
